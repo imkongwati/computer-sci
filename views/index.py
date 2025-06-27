66ddae48124796aad9011cb7c426388fe2858f58
imkongwati <rockstarsfun@gmail.com>
2025-06-27 16:13:10 +0530

    blank

--- views/index.py
+++ views/index.py
@@ -30,10 +30,27 @@ def IndexView(page:ft.Page, params):
 
 
     appbar = CreateAppBar()
+    question_data={"questions" :" Name 5 largest country",
+            "answers" : ["Russia","china","india","USA","canada","brazil"]}
+    question_tb= ft.Text(value=question_data["questions"],size=54)
+    answers_column=ft.Column()
+    i=1
+    for answer in question_data["answers"]:
+        number=ft.Container(content=ft.Text(value=i,bgcolor=ft.Colors.SECONDARY_CONTAINER,width=18,height=20,
+                                            alignment= ft.alignment.center,
+                                            border_radius= 16))
 
+        a=ft.Text(value=answer,size=20)
+        row=ft.Row(controls=number)
+        answers_column.controls.append(row)
+
+        i+=1
+
+
+    user_answer_tf= ft.TextField(value="type here")
     page.views.append(ft.View(
         "/",
-        [appbar],
+        [appbar,question_tb,answers_column],
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
 
     )
